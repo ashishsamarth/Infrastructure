3af866d829dd6cc2f7bae700ba173c8cf2c5360c
ashish samarth <mrashishsamarth@gmail.com>
2023-03-01 15:51:58 -0600

    001-create-s3-bucket

---
+++ Terraform/AWS/001-Connect-To-AWS-Create-S3-Bucket/001-create-s3-bucket.tf
@@ -0,0 +1,36 @@
+# Provider can be defined in two ways
+
+# The following method works, when you read the 'access-key', 'secret-key' and 'default-region' from the user(s) .aws/credentials and .aws/config file
+# if you have multiple profiles in "aws configure, then the format will be slightly different"
+
+provider "aws" {}
+
+# Another method is to define the access_key, secret_key and region with in the provider section, but this is not a recommended method
+/*
+provider "aws" {
+    region = "your-region-here"
+    access_key = "your-access-key-here"
+    secret_key = "your-secret-key-here"
+}
+
+*/
+
+/*  
+Now for example you have to create a resource - for e.g. S3 bucket
+Use, the following syntax
+
+For S3 - "my-terraform-bucket" is the resource name for the S3 bucket in terraform
+Actual name of the bucket will be - samarth-terraform-bucket
+
+Note: This is a non-versioned and private bucket
+
+*/
+
+resource "aws_s3_bucket" "my-terraform-bucket" {
+    bucket = "samarth-terraform-bucket"
+    acl = "private"
+
+    tags = {
+        Name: "Created-By-Terraform"
+    }
+}
